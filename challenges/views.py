7f7904151b1a21022cec090e2e75ffb52021835e
Vladyslav Tyshchenko <vlad.tiw27@gmail.com>
2025-01-12 12:44:05 +0100

    Switch to `render` for templating and handle 404s with exceptions
    Replaced `render_to_string` and `HttpResponse` with `render` for simplicity and consistency in views. Updated 404 handling to raise `Http404` instead of returning `HttpResponseNotFound`. Added a custom 404.html template for improved error page display.

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
+
 
 monthly_challenges = {
     "january": "Eat no meat for the entire month!",
@@ -36,7 +36,6 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = render_to_string("challenges/challenge.html", {"text": challenge_text, "month_name": month})
-        return HttpResponse(response_data, content_type="text/html")
+        return render(request, "challenges/challenge.html", {"text": challenge_text, "month_name": month})
     except:
-        return HttpResponseNotFound("<h1>This month is not supported.</h1>")
+        raise Http404()
